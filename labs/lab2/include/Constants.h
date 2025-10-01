77c5b8aa5b4dd31dbcacac59bb842bc820d841e5
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-10-01 14:07:52 +0300

    feat: add constant values for all lab.

---
+++ labs/lab2/include/Constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <vector>
+#include <string>
+
+namespace Constants {
+	const int minNumberOfVector = 0;
+	const int maxNumberOfVector = 100;
+
+	const std::vector<int> longOfVector = {10000, 100000, 1000000, 10000000};
+
+	const int numberOfThreads = 10;
+
+	const std::string folder("plots/files/");
+}
+
+#endif
